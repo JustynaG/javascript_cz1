3c38446fee7de016a67ef76fd78c28ea5f137da9
Justyna G <justykag@wp.pl>
2017-07-03 21:22:50 +0200

    zadanie piąte

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,22 @@ blokowy
 console.log('Hello, ' + name);
 
 var a = 5;
-	h = 7;
-	triangleArea = a * h / 2;
+var b = 7;
+var	triangleArea = a * b / 2;
 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + b + ' is equal to: ' + triangleArea);
 
 alert(triangleArea);
+
+
+var value = (a * a) + (2 * a * b) - (b * b);
+
+console.log(value);
+
+if (value < 0) {
+	console.log('Wartość ujemmna');
+} else if (value > 0) {
+	console.log('Wartość dodatnia');
+} else { 
+	console.log('Wartość = 0');
+}
